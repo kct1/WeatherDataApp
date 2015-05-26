8a4c4b6468888ed744d8125236a1417668422885
Kyle T <KyleTuczynski@Kyles-iMac.local>
2015-05-26 19:30:32 -0400

    Initial Commit

---
+++ WeatherDataLoader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherDataLoader
+//
+//  Created by Kyle Tuczynski on 5/26/15.
+//  Copyright (c) 2015 Kyle Tuczynski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeatherDataLoader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherDataLoader
+//
+//  Created by Kyle Tuczynski on 5/26/15.
+//  Copyright (c) 2015 Kyle Tuczynski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
